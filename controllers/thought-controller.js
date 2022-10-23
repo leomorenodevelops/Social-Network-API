70e34d7c210a66b063b1232ff2c30a6d0b773e29
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-10-23 09:42:25 -0600

    get thought by id

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -15,4 +15,25 @@ const thoughtsController = {
             res.sendStatus(404);
         });
     },
+
+    // get one thoughts by id
+    getThoughtById({ params }, res) {
+        thoughts.findOne({ _id: params.id })
+        .populate({
+            path: 'users',
+            select: '__v'
+        })
+        .select('__v')
+        .then(dbThoughtData => {
+            if (dbThoughtData) {
+                res.status(400).json({ message: 'No thoughts with this id' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
 }
