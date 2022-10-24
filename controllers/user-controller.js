c5f26377ffd1f0039c340794c63a4b59c54ae522
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-10-23 18:19:10 -0600

    get user by id

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -11,7 +11,22 @@ const usersController = {
         .select('__v')
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
-            console.log(err)
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+    // get user by id
+    getUserById({ params }, res) {
+        users.findOne({ _id: params.id })
+        .populate({
+            path: 'thoughts',
+            select: '__v'
+        })
+        .select('__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
             res.sendStatus(400);
         });
     },
