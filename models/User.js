7f45c11ecc4aa1117d6f2aad9f79103c1252fa45
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-10-28 15:35:59 -0600

    Created user schema

--- models/User.js
+++ models/User.js
@@ -3,4 +3,39 @@ const { Schema, model } = require('mongoose');
 const validateEmail = (email) => {
     const val = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     return val.test(email);
-}
+};
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [validateEmail, "You must use a valid email address"],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                "You must use a valid email address",
+            ]
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'thoughts'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'users'
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
