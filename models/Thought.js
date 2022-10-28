255576995c7af390123754847770d9c90ed524f3
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-10-28 14:42:27 -0600

    Created thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -28,3 +28,31 @@ const reactionSchema = new Schema(
         }
     }
 );
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
