bdf9b8d52c21d8c4b9de7cfc93ddd06c01fbb7ba
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-10-29 15:25:24 -0600

    Set up routes for get all and get by id for thoughts

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+} = require('../../controllers/thought-controller');
+
+// Set up get all and post for thoughts
+router.route('/').get(getAllThoughts).post(createThought);
+// Set up get by id, put and delete for thoughts
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
