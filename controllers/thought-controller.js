84ace2c1eff7ffe06e505a36111b17a07e7b1453
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-11-03 17:00:48 -0600

    Updated delete thought function and created create reaction function

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -6,13 +6,13 @@ const thoughtsController = {
         thoughts.find({})
         .populate({
             path: 'users',
-            select: '__v'
+            select: '-__v'
         })
-        .select('__v')
+        .select('-__v')
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
-            res.sendStatus(404);
+            res.sendStatus(400);
         });
     },
 
@@ -21,12 +21,12 @@ const thoughtsController = {
         thoughts.findOne({ _id: params.id })
         .populate({
             path: 'users',
-            select: '__v'
+            select: '-__v'
         })
-        .select('__v')
+        .select('-__v')
         .then(dbThoughtData => {
             if (!dbThoughtData) {
-                res.status(400).json({ message: 'No thoughts with this ID' });
+                res.status(404).json({ message: 'No thoughts with this ID' });
                 return;
             }
             res.json(dbThoughtData);
@@ -73,7 +73,37 @@ const thoughtsController = {
                 res.status(404).json({ message: 'No thought with this ID' });
                 return;
             }
+            return users.findOneAndUpdate({ _id: parmas.userId }, { $pull: { thoughts: params.Id } }, { new: true }
+            )
         })
+        .then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No user found with this ID!' });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch(err => res.json(err));
+    },
+
+    createReaction({ params, body }, res) {
+        thoughts.findOneAndUpdate({ _id: params.thoughtId }, { $push: { reactions: body}}, { new: true, runValidators: true})
+        .populate({
+            path: 'users', 
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No thoughts with this ID' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
     }
 };
 
