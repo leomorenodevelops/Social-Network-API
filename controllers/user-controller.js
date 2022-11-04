4bbdea191a70ea2f134f6cc00c5b2673870150db
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-11-04 17:16:54 -0600

    Created deletefriend function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -85,6 +85,17 @@ const usersController = {
         })
         .catch((err) => res.status(400).json(err));
     },
+    deleteFriend({ params }, res) {
+        users.findOneAndUpdate({ _id: params.userId }, { $pull: { friends: params.friendId } }, { new: true })
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No user found with this ID' });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => res.status(400).json(err));
+      }
 };
 
 module.exports = usersController;
