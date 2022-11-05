be9ecf9a23239e9fc27965ce47e2912a9dbaaedc
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-11-04 18:18:22 -0600

    Added route for post and delete friends

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,12 +5,16 @@ const {
     getUserById,
     createUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    addFriend,
+    deleteFriend
 } = require('../../controllers/user-controller');
 
 // Set up get all and post for users
 router.route('/').get(getAllUsers).post(createUser);
 // Set up get by id, put and delete for users
 router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+// Set up post and delete for friends
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
